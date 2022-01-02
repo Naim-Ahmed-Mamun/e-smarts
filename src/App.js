229c2c9f845692b320df67d5476c0105eee5b8fd
proloypaul <proloyp23@gmail.com>
2022-01-02 22:48:29 +0600

    added pricing Component from utsha

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import PricingBox from './components/PricingBox/PricingBox';
 import Home from './pages/Home/Home/Home';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home></Home>}></Route>
         </Routes>
+        <PricingBox></PricingBox>
       </BrowserRouter>
     </>
   );
