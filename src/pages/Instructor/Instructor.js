d8b6b940d3a794b5e0637eb50a074e10aae4510b
anisur6 <armunna223@gmail.comm>
2022-01-03 20:16:57 +0600

    instructor component added

---
+++ src/pages/Instructor/Instructor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './instructor.css'
+
+const Instructor = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet.</h1>
+        </div>
+    );
+};
+
+export default Instructor;
