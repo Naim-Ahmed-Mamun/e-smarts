4c227b805ea9164cf264e18a29afe8c85a644e35
Naim-Ahmed-Mamun <naimahmod852@gmail.com>
2022-01-03 18:20:45 +0600

    all courses section add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/shared/Login/Login/Login';
+import Register from './components/shared/Register/Register';
+import SingleCourseDetails from './components/SingleCourseDetails/SingleCourseDetails';
+import ContextProvider from './context/ContextProvider';
+import AllCourses from './pages/AllCourses/AllCourses/AllCourses';
 import Home from './pages/Home/Home/Home';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
+      <ContextProvider>
+        <BrowserRouter>
+          <Routes>
             <Route path="/" element={<Home></Home>}></Route>
-        </Routes>
-      </BrowserRouter>
+            <Route path="/home" element={<Home></Home>}></Route>
+            <Route path="/allCourses" element={<AllCourses></AllCourses>}></Route>
+            <Route path="/courses/:name" element={<SingleCourseDetails></SingleCourseDetails>}></Route>
+            <Route path="/login" element={<Login></Login>}></Route>
+            <Route path="/register" element={<Register></Register>}></Route>
+          </Routes>
+        </BrowserRouter>
+      </ContextProvider>
     </>
   );
 }
---
+++ src/components/shared/Login/Login/Login.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import loginImg from '../../../../img/login-img.svg';
+import { useForm } from "react-hook-form";
+import './Login.css';
+import { Link,useLocation,useNavigate } from 'react-router-dom';
+import useAuth from '../../../../hooks/useAuth';
+import Navigation from '../../Navigation';
+
+const Login = () => {
+   // use form
+   const { register, handleSubmit, } = useForm();
+   // auth
+   const {loginUser} = useAuth()
+   // location
+   const location = useLocation()
+   // history
+   const navigate = useNavigate()
+   // submit
+   const onSubmit = data => {
+      // console.log(data)
+      loginUser(data.email,data.password,location,navigate)
+   };
+
+   return (
+      <>
+      <Navigation></Navigation>
+         <div className="login">
+            <div className="container">
+               <div className="row align-items-center">
+                  <div className="col-lg-6" data-aos="fade-up">
+                     <div className="login_img">
+                        <img src={loginImg} alt="" />
+                     </div>
+                  </div>
+                  <div className="col-lg-6" data-aos="fade-up">
+                     <div className="form_container">
+                     <h2 className="text-center mb-4">Login</h2>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                           <input type="email" {...register("email")} placeholder="Email" />
+                           <input type="password" {...register("password")} placeholder="Password" />
+                           <input type="submit" value="Login" />
+                        </form>
+                        <p className="text-center">New User ? <Link to="/register">Please Register</Link></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </>
+   );
+};
+
+export default Login;
