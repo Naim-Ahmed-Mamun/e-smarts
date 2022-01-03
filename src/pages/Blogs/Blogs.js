9cfdedf11f823f9ea2c5ad200a3af298648e1d78
proloypaul <proloyp23@gmail.com>
2022-01-03 17:50:29 +0600

    complete blog section

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import "./Blogs.css";
 
 const Blogs = () => {
-    // const [bolgData, setBlogsData] = useState([])
+    const [blogsData, setBlogsData] = useState([])
 
-    // useEffect((
-    //     fetch("https://limitless-shelf-68611.herokuapp.com/blogs?fbclid=IwAR0ve1PrFFJqwxWMKtkpG204RhZv6kecuKPAOVr8F1lbPM4bphMYgfXswg0")
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data)
-    //             setBlogsData(data)
-    //         })
+    useEffect(() => {
+        const url = `https://limitless-shelf-68611.herokuapp.com/blogs?fbclid=IwAR0ve1PrFFJqwxWMKtkpG204RhZv6kecuKPAOVr8F1lbPM4bphMYgfXswg0`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data)
+                setBlogsData(data)
+            })
 
-    // ), [])
+    }, [])
 
     return (
         <div className='blogContainer'>
@@ -23,8 +24,26 @@ const Blogs = () => {
                 </Container>
             </div>
             <Container>  
-                <div>
-
+                <div >
+                    <div>
+                        {blogsData.map(blog => <div key={blog._id} className='blogs'>
+                            <div className='blogBox'>
+                                <div>
+                                    <img src={blog.img} alt='Empty' style={{width: "100%"}}/>
+                                </div>
+                                <div className='blog-content'>
+                                    <h1>{blog.title}</h1>
+                                    <div className='blog-reaction'>
+                                        <p>{blog.date}</p>
+                                        <p><i class="far fa-heart"></i> 20</p>
+                                        <p><i class="fas fa-comment"></i> 5</p>
+                                        <p>Education</p>
+                                    </div>
+                                    <p>{blog.description}</p>
+                                </div>
+                            </div>
+                        </div>)}
+                    </div>
                 </div>
             </Container>
         </div>
