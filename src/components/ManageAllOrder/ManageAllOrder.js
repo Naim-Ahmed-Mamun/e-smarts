a72ddaa715eb46bbd6e06b60c9733b4ee3748da6
proloypaul <proloyp23@gmail.com>
2022-01-04 19:46:52 +0600

    create manageAllOrder functionality

--- src/components/ManageAllOrder/ManageAllOrder.js
+++ src/components/ManageAllOrder/ManageAllOrder.js
@@ -7,7 +7,7 @@ const ManageAllOrder = () => {
     const [userOrder, setUserOrder] = useState([])
 
     useEffect(() => {
-        const url = `https://limitless-shelf-68611.herokuapp.com/orders?fbclid=IwAR0C6pDCutxiptLX-RqtDh8CstjvsdhQc0vKrSYMmjEc1UDlrOgBeB_ojcY`
+        const url = `https://limitless-shelf-68611.herokuapp.com/orders`
         fetch(url)
             .then(res => res.json())
             .then(data => {
@@ -15,11 +15,23 @@ const ManageAllOrder = () => {
                 setUserOrder(data)
             })
     }, [])
+
+    const handleDelete = id => {
+        console.log(id)
+        const url = `https://limitless-shelf-68611.herokuapp.com/orders/${id}`
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
     return (
         <div className='MakeAdminContainer'>
             <div className='MakeAdminTitle'>
                 <Container>
-                    <h1>Make An Admin</h1>
+                    <h1>Make All Order</h1>
                     <Link to="/home"><p>Home/Make All Order</p></Link>
                 </Container>
             </div>
@@ -35,13 +47,13 @@ const ManageAllOrder = () => {
                             <div className='col-lg-2 col-md-4 col-sm-6'><h1>Delete</h1></div>
                         </div>
                     </div>
-                    {userOrder.map(order => <div key={order.course._id} className='row userOrder'>
+                    {userOrder.map(order => <div key={order._id} className='row userOrder'>
                         <div className='col-lg-2 col-md-4 col-sm-6'><i className="fas fa-user"></i> {order.name}</div>
                         <div className='col-lg-2 col-md-4 col-sm-6'>{order.email}</div>
                         <div className='col-lg-2 col-md-4 col-sm-6'>{order.course.type}</div>
-                        <div className='col-lg-2 col-md-4 col-sm-6'>{order.price}</div>
+                        <div className='col-lg-2 col-md-4 col-sm-6'>${order.price}</div>
                         <div className='col-lg-2 col-md-4 col-sm-6'><button>Pay</button></div>
-                        <div className='col-lg-2 col-md-4 col-sm-6'><button><i className="fas fa-trash"></i></button></div>
+                        <div className='col-lg-2 col-md-4 col-sm-6'><button onClick={() => handleDelete(order.course._id)}><i className="fas fa-trash"></i></button></div>
                     </div>)}
                 </div>
             </Container>
