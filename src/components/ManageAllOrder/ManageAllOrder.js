7a6104f5a8b46d10926586bfba982858a815b99c
proloypaul <proloyp23@gmail.com>
2022-01-04 18:21:48 +0600

    Manage all order styled

--- src/components/ManageAllOrder/ManageAllOrder.js
+++ src/components/ManageAllOrder/ManageAllOrder.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './ManageAllOrder.css'
 
 const ManageAllOrder = () => {
     const [userOrder, setUserOrder] = useState([])
@@ -10,7 +11,7 @@ const ManageAllOrder = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+                // console.log(data)
                 setUserOrder(data)
             })
     }, [])
@@ -23,8 +24,25 @@ const ManageAllOrder = () => {
                 </Container>
             </div>
             <Container>
-                <div>
-
+                <div className='userOrders'>
+                    <div>
+                        <div className='row order-title'>
+                            <div className='col-lg-2 col-md-4 col-sm-6'><h1>Name</h1></div>
+                            <div className='col-lg-2 col-md-4 col-sm-6'><h1>Email</h1></div>
+                            <div className='col-lg-2 col-md-4 col-sm-6'><h1>Type</h1></div>
+                            <div className='col-lg-2 col-md-4 col-sm-6'><h1>Price</h1></div>
+                            <div className='col-lg-2 col-md-4 col-sm-6'><h1>Pay</h1></div>
+                            <div className='col-lg-2 col-md-4 col-sm-6'><h1>Delete</h1></div>
+                        </div>
+                    </div>
+                    {userOrder.map(order => <div key={order.course._id} className='row userOrder'>
+                        <div className='col-lg-2 col-md-4 col-sm-6'><i className="fas fa-user"></i> {order.name}</div>
+                        <div className='col-lg-2 col-md-4 col-sm-6'>{order.email}</div>
+                        <div className='col-lg-2 col-md-4 col-sm-6'>{order.course.type}</div>
+                        <div className='col-lg-2 col-md-4 col-sm-6'>{order.price}</div>
+                        <div className='col-lg-2 col-md-4 col-sm-6'><button>Pay</button></div>
+                        <div className='col-lg-2 col-md-4 col-sm-6'><button><i className="fas fa-trash"></i></button></div>
+                    </div>)}
                 </div>
             </Container>
         </div>
