0108530144b9f6e47321ae3f7d141b87b39e6988
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2022-01-04 23:30:11 +0600

    hello

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Home from './pages/Home/Home/Home';
 function App() {
   return (
     <>
+      {/* hellO */}
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/" element={<Home></Home>}></Route>
         </Routes>
       </BrowserRouter>
     </>
